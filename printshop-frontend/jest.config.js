e8f296a7fdc709a113d5b38d2d3faefdcbbbfb0a
[Johnny Kwan] <[jkkwan1@ualberta.ca]>
2026-05-06 19:45:16 -0400

    Updated github workflows for frontend testing;

--- printshop-frontend/jest.config.js
+++ printshop-frontend/jest.config.js
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({
 
 const config = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ['./printshop-frontend/jest.setup.js'],
+  setupFilesAfterEnv: ['jest.setup.js'],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
