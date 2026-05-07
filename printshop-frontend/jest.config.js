1b32ca8f0c393d9f19bd3ddbba241178193a467f
robelmeasho <cx0987@hotmail.com>
2026-05-07 17:10:12 -0600

    4

--- printshop-frontend/jest.config.js
+++ printshop-frontend/jest.config.js
@@ -1,12 +1,12 @@
-const path = require("path");
-
 module.exports = {
+  rootDir: ".",
+
   testEnvironment: "jsdom",
 
-  setupFilesAfterEnv: [path.join(__dirname, "jest.setup.js")],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 
   moduleNameMapper: {
-    "^@/(.*)$": path.join(__dirname, "$1"),
+    "^@/(.*)$": "<rootDir>/$1",
   },
 
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
