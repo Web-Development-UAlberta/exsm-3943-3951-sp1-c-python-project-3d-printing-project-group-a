676e3f391019dcb56647b55e49f1350766f25203
[Johnny Kwan] <[jkkwan1@ualberta.ca]>
2026-05-07 19:43:28 -0400

    Updated jest.config.js; Updated db_task.yml;

--- printshop-frontend/jest.config.js
+++ printshop-frontend/jest.config.js
@@ -1,5 +1,4 @@
 const nextJest = require("next/jest");
-
 const path = require('path');
 
 const createJestConfig = nextJest({
@@ -7,8 +6,8 @@ const createJestConfig = nextJest({
 });
 
 const config = {
-  testEnvironment: "jsdom",
   setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.js')],
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
