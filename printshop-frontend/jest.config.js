a89a0fb18c2e99aad26302c3c67fb8b5ffdbeb5b
[Johnny Kwan] <[jkkwan1@ualberta.ca]>
2026-05-07 19:28:37 -0400

    Updated jest.config.js;

--- printshop-frontend/jest.config.js
+++ printshop-frontend/jest.config.js
@@ -1,12 +1,14 @@
 const nextJest = require("next/jest");
 
+const path = require('path');
+
 const createJestConfig = nextJest({
   dir: "./",
 });
 
 const config = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.ts')],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
