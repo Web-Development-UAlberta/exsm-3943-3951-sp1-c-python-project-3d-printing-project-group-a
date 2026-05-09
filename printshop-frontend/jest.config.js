fb70210feaa35f4330c7ce454383835bb85d7706
robelmeasho <cx0987@hotmail.com>
2026-05-09 16:54:05 -0600

    changed config file

--- printshop-frontend/jest.config.js
+++ printshop-frontend/jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   rootDir: ".",
-
   testEnvironment: "jsdom",
 
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
-
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
 };
