f3c28f59c4b29ce3833715fc732d3398540fa5b7
rami779 <101010ramirami707070@gmail.com>
2026-05-10 00:45:55 +0300

    Fixing .yml, src/app.database.py

--- src/app/database.py
+++ src/app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
     'mysql+mysqldb://root:ramidatabase@localhost:3306/3d_printing_project',
+    connect_args={"host": "127.0.0.1"},
     echo=True
 )
 
