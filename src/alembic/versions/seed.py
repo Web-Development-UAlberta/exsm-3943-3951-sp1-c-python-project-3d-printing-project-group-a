371602cb9c8e6d5374ea1f050b1662f11c1d9d69
rami779 <101010ramirami707070@gmail.com>
2026-05-10 01:29:49 +0300

    fixes in seed migration

--- src/alembic/versions/seed.py
+++ src/alembic/versions/seed.py
@@ -1,6 +1,4 @@
-from alembic import op 
-import sqlalchemy as sa
-from app.models import User, Filament, PrinterType, Printer, Tag, Model, ModelFilament, OrderHeader, OrderDetail
+from alembic import op
 
 revision = '1234567890'
 down_revision = '0cd4f77e0e19'
@@ -8,49 +6,41 @@ branch_labels = None
 depends_on = None
 
 def upgrade():
-    # Users
     op.execute("""
-    INSERT INTO Users(username, full_name, phone_number, city, street_address, province, postal_code, is_admin)
+    INSERT INTO users(username, full_name, phone_number, city, street_address, province, postal_code, is_admin)
     VALUES ('admin', 'Bo Cen', '000-111-2222', 'Edmonton', '123 Main St', 'AB', 'T6G 2G5', TRUE);
     """)
 
-    # Filament
     op.execute("""
-    INSERT INTO Filament(material_name, color_hex, quantity_in_stock,
-    manufacturer, more_wear_and_tear, finish_filament, filament_price)
+    INSERT INTO filament(material_name, color_hex, quantity_in_stock, manufacturer, more_wear_and_tear, finish_filament, filament_price)
     VALUES ('PLA', '#FFFFFF', 100, 'm3D', 10.00, 'Satin', 18.00);
     """)
 
-    # Printer Type
     op.execute("""
-    INSERT INTO Printer_Type(printer_name, max_size)
+    INSERT INTO printer_type(printer_name, max_size)
     VALUES ('Prusa MK4', 500.00);
     """)
 
-    # Printer
     op.execute("""
-    INSERT INTO Printer(printer_type_id, filament_id)
+    INSERT INTO printer(printer_type_id, filament_id)
     VALUES (1, 1);
     """)
 
-    # Tag
     op.execute("""
-    INSERT INTO Tag(tag_name)
+    INSERT INTO tag(tag_name)
     VALUES ('Gaming');
     """)
-
-    # Model
+    
     op.execute("""
-    INSERT INTO Model(model_name, model_length, model_width, model_height,
-    model_description, tag_id, printer_id)
+    INSERT INTO model(model_name, model_length, model_width, model_height, model_description, tag_id, printer_id)
     VALUES ('Iron Man', 10, 10, 10, 'Iron Man Model', 1, 1);
     """)
 
 
 def downgrade():
-    op.execute("DELETE FROM Model WHERE model_name = 'Iron Man';")
-    op.execute("DELETE FROM Tag WHERE tag_name = 'Gaming';")
-    op.execute("DELETE FROM Printer WHERE printer_id = 1;")
-    op.execute("DELETE FROM Printer_Type WHERE printer_name = 'Prusa MK4';")
-    op.execute("DELETE FROM Filament WHERE material_name = 'PLA';")
-    op.execute("DELETE FROM Users WHERE username = 'admin';")
+    op.execute("DELETE FROM model WHERE model_name = 'Iron Man';")
+    op.execute("DELETE FROM tag WHERE tag_name = 'Gaming';")
+    op.execute("DELETE FROM printer WHERE printer_id = 1;")
+    op.execute("DELETE FROM printer_type WHERE printer_name = 'Prusa MK4';")
+    op.execute("DELETE FROM filament WHERE material_name = 'PLA';")
+    op.execute("DELETE FROM users WHERE username = 'admin';")
