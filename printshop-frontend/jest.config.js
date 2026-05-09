d5afdab3acf753d41c9744db61c9ad3504678975
robelmeasho <cx0987@hotmail.com>
2026-05-09 17:41:24 -0600

    added babelrc and updated config

--- printshop-frontend/jest.config.js
+++ printshop-frontend/jest.config.js
@@ -1,16 +1,4 @@
-const nextJest = require("next/jest");
-const path = require('path');
-
-const createJestConfig = nextJest({
-  dir: "./",
-});
-
-const config = {
-  setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.js')],
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
-  },
-};
-
-module.exports = createJestConfig(config);
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+}
